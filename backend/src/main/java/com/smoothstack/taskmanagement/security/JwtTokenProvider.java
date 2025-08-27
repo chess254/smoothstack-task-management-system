912e30ca1af3c5f238e3f8d3a6bd699a44871134
chess254 <lae2006a@yahoo.com>
2025-08-27 19:01:41 +0000

    fix auth

--- backend/src/main/java/com/smoothstack/taskmanagement/security/JwtTokenProvider.java
+++ backend/src/main/java/com/smoothstack/taskmanagement/security/JwtTokenProvider.java
@@ -25,13 +25,13 @@ public class JwtTokenProvider {
     }
 
     public Long getUserIdFromJWT(String token) {
-        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parser().setSigningKey(key).build().parseClaimsJws(token).getBody();
         return Long.parseLong(claims.getSubject());
     }
 
     public boolean validateToken(String authToken) {
         try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(authToken);
+            Jwts.parser().setSigningKey(key).build().parseClaimsJws(authToken);
             return true;
         } catch (Exception e) {
             return false;
